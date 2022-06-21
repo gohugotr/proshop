9839f7acb562acafebd329397306ca7069cc0014
Cengiz YILMAZ <terabilgi@gmail.com>
2022-06-21 03:35:58 +0300

    font-avesome 6 yüklendi

--- frontend/src/components/HeaderPro.jsx
+++ frontend/src/components/HeaderPro.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Container,Navbar,Nav } from 'react-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const HeaderPro = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container >
+        <Container>
           <Navbar.Brand href='/'>ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls='navbarScroll' />
           <Navbar.Collapse id='navbarScroll'>
             <Nav className='me-auto my-2 my-lg-0' style={{ maxHeight: '100px' }} navbarScroll>
-              <Nav.Link href='/cart'>Cart</Nav.Link>
-              <Nav.Link href='/login'>Sign In</Nav.Link>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart me-1'></i>Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user me-1'></i>Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
