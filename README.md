41658b78414f1ef1bb92999d4775d0a199f88254
Cengiz YILMAZ <terabilgi@gmail.com>
2022-06-21 13:11:42 +0300

    route yapılandırılıyor

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 [MERN eCommerce From Scratch](https://www.udemy.com/course/mern-ecommerce/learn/lecture/22484760#overview)
 
+
+[What’s new in React Router V6](https://medium.com/@manishsundriyal/whats-new-in-react-router-v6-20eefe665be9)
+
 // 6. Ders Dakika 4.40 ta kalındı / Toplam 92 ders 15 saat
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,23 @@ import HeaderPro from './components/HeaderPro'
 import FooterPro from './components/FooterPro'
 import {Container} from 'react-bootstrap'
 import HomeScreen from './screen/HomeScreen'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductScreen from './screen/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <HeaderPro />
       <main className='py-3'>
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <FooterPro />
-    </>
+    </Router>
   )
 }
 
