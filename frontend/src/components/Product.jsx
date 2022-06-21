2b97480faed99058649a69084527eca01a1d7b84
Cengiz YILMAZ <terabilgi@gmail.com>
2022-06-21 11:27:08 +0300

    Product devam

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import {Card} from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
-
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' >
-        </Card.Img>
+        <Card.Img src={product.image} variant='top'></Card.Img>
       </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as='div'>
+            <Rating/>
+        </Card.Text>
+      </Card.Body>
     </Card>
   )
 }
