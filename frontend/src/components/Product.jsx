9377cf4cf1ae331247c1963b9533d25c3acccdd1
Cengiz YILMAZ <terabilgi@gmail.com>
2022-06-21 12:12:20 +0300

    Rating bileşeni inşa edildi

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -14,8 +15,12 @@ const Product = ({ product }) => {
           </Card.Title>
         </a>
         <Card.Text as='div'>
-            <Rating/>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} color='red' />
+          {/* <div className='my-3'>
+            {product.rating} from {product.numReviews} reviews
+          </div> */}
         </Card.Text>
+        <Card.Text as='h3'>₺{product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
